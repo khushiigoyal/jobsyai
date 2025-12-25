44def30814cebbc4d0fd1cafef490fb9bbe714f1
khushiigoyal <khushigoyal0508452@gmail.com>
2025-12-25 18:12:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/api/generate', async (req, res) => {
     console.log("📩 Request received from website...");
     try {
         const { prompt } = req.body;
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
